f255f65f7e2dda03fa4ecb21fc2960ade3e07ec3
Aymane Elmadidi <aymanelmadidi@gmail.com>
2023-01-06 19:21:01 +0100

    adding process.env.PORT

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,6 @@ async function bootstrap() {
   // app.enableCors();
   // app.use(helmet());
   app.use(cookieParser());
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
