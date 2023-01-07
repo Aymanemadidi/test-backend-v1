3d347073b4f91ca86919f40061f7c976860b8e0a
Aymane Elmadidi <aymanelmadidi@gmail.com>
2023-01-07 03:34:16 +0100

    testing chatgpt fixes

--- src/common/graphql.module.ts
+++ src/common/graphql.module.ts
@@ -1,6 +1,13 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+
+const corsOptions: CorsOptions = {
+  origin: ['https://frontend-test-v1-rho.vercel.app'],
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
 
 @Module({
   imports: [
@@ -10,15 +17,7 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
       playground: true,
       cache: 'bounded',
       context: ({ req, res }) => ({ req, res }),
-      cors: {
-        origin:
-          // process.env.NODE_ENV === 'production'
-          'https://frontend-test-v1-rho.vercel.app/',
-        // :
-        // 'http://localhost:5000',
-
-        credentials: true,
-      },
+      cors: corsOptions, // enable CORS
     }),
   ],
 })
